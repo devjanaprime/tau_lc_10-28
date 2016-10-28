e146ed1f1a14cbcf6d2b2be1574455df92ca3d2f
devjanaprime <dev@primeacademy.io>
2016-10-28 13:20:55 -0500

    added boring stuff

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ var funFunFunction = function () {
   var theFunThing = prompt( 'What\'s so funny?' );
   console.log( 'the user thinks', theFunThing, 'is funny...' );
   var pTag = document.getElementById( 'funnyStuff' );
-  pTag.appendChild(document.createTextNode( theFunThing ) );
+  pTag.appendChild( document.createTextNode( theFunThing ) );
+};
+
+var boringBoringFunction = function(){
+  console.log( 'in boringBoringFunction' );
+    var theBoringThing = prompt( 'What\'s boring?' );
+    console.log( 'the user thinks', theBoringThing, 'is boring and I agree so I\'m not going to show it on the page...' );
 };
